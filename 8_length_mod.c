3128cdc224563a103eb1ce5444c41ae5fac61705
Siddami <dharmmy621@gmail.com>
2024-02-02 06:34:24 -0800

    8_length_mod.c

--- 8_length_mod.c
+++ 8_length_mod.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* handle_print - Prints an argument based on its type
-* @fmt: Formatted string in which to print the arguments.
-* @list: List of arguments to be printed.
+* handle_print - Prints an argument according to its data type.
+* @fmt: A formatted string in which to print the arguments.
+* @list: A list of arguments to be printed.
 * @ind: ind.
-* @buffer: Buffer array to handle print.
-* @flags: Calculates active flags
+* @buffer: A Buffer array to handle print.
+* @flags: Determines active flags
 * @width: get width.
 * @precision: Precision specification
 * @size: Size specifier
